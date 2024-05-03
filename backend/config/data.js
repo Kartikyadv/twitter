ce9132d600e5c9df836b02ea32c08213f9e26241
Kartik Yadav <ky5986390@gmail.com>
2024-05-03 13:43:46 +0530

    backend cors update

---
+++ backend/config/data.js
@@ -0,0 +1,1 @@
+// https://twitter-mujz.onrender.com
--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:[process.env.PROD_FRONTEND_URL,process.env.DEV_FRONTEND_URL],
     credentials:true
 }
 app.use(cors(corsOptions));
